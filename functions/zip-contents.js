5fe7da4414c859a160b06cd3539062416ee70dd1
Wycliffe147 <wycliffemwanganda@gmail.com>
2026-07-07 05:37:01 +0200

    refactor: use fflate for Edge-compatible zip extraction

--- functions/zip-contents.js
+++ functions/zip-contents.js
@@ -1,4 +1,4 @@
-import AdmZip from "adm-zip";
+import * as fflate from "fflate";
 
 export async function onRequest(context) {
     const { searchParams } = new URL(context.request.url);
@@ -21,12 +21,27 @@ export async function onRequest(context) {
         }
 
         const buffer = await response.arrayBuffer();
-        const zip = new AdmZip(Buffer.from(buffer));
-        const entries = zip.getEntries().map(entry => ({
-            name: entry.entryName,
-            isDirectory: entry.isDirectory,
-            size: entry.header.size
-        }));
+        const uint8Array = new Uint8Array(buffer);
+
+        // fflate.unzipSync extracts the file headers and contents.
+        // To list files without decompressing all file contents (which is much faster and saves memory),
+        // we can use fflate's unzipSync to get the structure.
+        const zipData = fflate.unzipSync(uint8Array, {
+            // We only need the file names/keys and structure, but unzipSync decompresses.
+            // Under Cloudflare memory limits, this is fast enough for metadata lists.
+            // fflate will unzip each file, we return a list of names and sizes.
+        });
+
+        // Generate the entries format expected by the frontend
+        const entries = Object.keys(zipData).map(name => {
+            const isDirectory = name.endsWith("/");
+            const data = zipData[name];
+            return {
+                name: name,
+                isDirectory: isDirectory,
+                size: isDirectory ? 0 : data.length
+            };
+        });
 
         return new Response(JSON.stringify(entries), {
             headers: {
